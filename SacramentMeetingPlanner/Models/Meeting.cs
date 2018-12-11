d658a1908403ce17b03f9f729e62824bfb543ad2
Lava84flow <Lava84flow@users.noreply.github.com>
2018-12-10 21:36:15 -0500

    Create Meeting.cs
    Set Model for a meeting

---
+++ SacramentMeetingPlanner/Models/Meeting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;    
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Meeting
+    {
+
+        public int ID { get; set; }
+
+        public string Bishopric { get; set; }  
+
+        [DataType(DataType.Date)]
+        public DateTime UpdatedDate { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime MeetingDate { get; set; }
+
+        public string Song { get; set; }
+
+        public string Prayers { get; set; }
+
+        public string Speaker { get; set; }
+        
+        public string Subject { get; set; }
+
+    }
+}
