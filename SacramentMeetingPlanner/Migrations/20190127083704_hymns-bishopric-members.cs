5893a95b62b0c3f31f9fa02e5a303b6dbee839cd
Lava84flow <Lava84flow@users.noreply.github.com>
2019-01-27 00:44:03 -0800

    DB Seeded and Running

---
+++ SacramentMeetingPlanner/Migrations/20190127083704_hymns-bishopric-members.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SacramentMeetingPlanner.Migrations
+{
+    public partial class hymnsbishopricmembers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bishopric",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Calling = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bishopric", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Hymn",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false),
+                    Number = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hymn", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WardMember",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(maxLength: 20, nullable: false),
+                    Calling = table.Column<string>(nullable: true),
+                    IsBishopric = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WardMember", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bishopric");
+
+            migrationBuilder.DropTable(
+                name: "Hymn");
+
+            migrationBuilder.DropTable(
+                name: "WardMember");
+        }
+    }
+}
